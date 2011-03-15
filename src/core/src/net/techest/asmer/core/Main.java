95d94086d41a716762bff03ce509b5e6c3006501
baizhongwei@gmail.com <baizhongwei@gmail.com@4c20ba55-bc57-e642-76be-e76673937a09>
2011-03-15 02:28:20 +0000

    更新寄存器模式

---
+++ src/core/src/net/techest/asmer/core/Main.java
@@ -0,0 +1,40 @@
+package net.techest.asmer.core;
+
+/*  Copyright 2010 princehaku
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Created on : 2011-3-6, 20:34:52
+ *  Author     : princehaku
+ */
+
+
+
+import net.techest.asmer.core.cpu.CPUBase;
+
+/**
+ *
+ * @author princehaku
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Asmer as=new Asmer();
+        CPUBase cpu =new CPU8086();
+        cpu.execute();
+    }
+
+}
